82d21672777548588287ed90724aa00fa2c7df98
Philani Ncube <ncbphi001@gmail.com>
2025-04-07 09:00:17 +0200

    feat(loading): enhance loading component with animated placeholders for better user experience

--- app/(dashboard)/dashboard/team/[team_id]/loading.tsx
+++ app/(dashboard)/dashboard/team/[team_id]/loading.tsx
@@ -18,6 +18,13 @@ const loading = () => {
 
     return (
         <div className="p-6">
+            <div className="flex justify-between">
+            <div className="flex flex-col">
+                <div className="h-6 bg-gray-200 rounded w-1/2 animate-pulse"></div>
+                <div className="h-4 bg-gray-200 rounded w-1/4 animate-pulse mt-2"></div>
+            </div>
+            <div className="h-8 bg-gray-200 rounded w-1/4 animate-pulse"></div>
+        </div>
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                 {metrics.map((metric, index) => (
                     <Card key={index} className="animate-pulse">
